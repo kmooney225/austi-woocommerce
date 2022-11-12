52c65eb9dba853c7006a99d5b7441320ef8ce10d
kmooney225 <kmooney225@gmail.com>
2022-11-12 02:47:49 -0700

    darkerning hero's tint

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () =>{
     return(
         <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover content'>
-            <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/65 z-[2]'>
+            <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]'>
 
             </div>
             <div className='p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]'>
